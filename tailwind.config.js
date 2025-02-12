76202177e9c64573c221ad754f0efef3d1fccf5d
AbdElrhmanKhalifa549 <abdelrhman.mkalifa@gmail.com>
2025-02-12 04:06:55 +0200

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      cb: ' #2F80ED',
+      cgray :' #F2F2F2',
+      t:'transparent'
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
